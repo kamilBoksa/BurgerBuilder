7c506339054bed8b3dc06025198fedeca4fcc6bb
F4tall <kamil.boksa@gmail.com>
2018-09-17 10:26:15 +0200

    Implemen user authentication

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -10,20 +10,19 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 class Orders extends Component {
 
     componentDidMount() {
-        this.props.onFetchOrders();
+        this.props.onFetchOrders(this.props.token, this.props.userId);
     }
 
     render() {
         let orders = <Spinner/>;
         if(!this.props.loading) {
             orders = this.props.orders.map(order => (
-                    <Order
-                        key={order.id}
-                        ingredients={order.ingredients}
-                        price={order.price}/>
-                ))
+                <Order
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={order.price}/>
+            ))
         }
-        console.log(orders);
         return (
             <div>
                 { orders }
@@ -34,13 +33,15 @@ class Orders extends Component {
 const mapStateToProps = state => {
     return {
         orders: state.order.orders,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token,
+        userId: state.auth.userId
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: () => dispatch(actions.fetchOrders())
+        onFetchOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
     }
 };
 
